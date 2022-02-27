3faf936d9e70ac135b1bcdb0182b6709073914b9
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-02-27 06:50:38 +0200

    add new project, add material ui, icon and create page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  // components: {
+  //   MuiButton: {
+  //     styleOverrides: {
+  //       root: {
+  //         fontSize: "1rem",
+  //       },
+  //     },
+  //   },
+  // },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Button from "@mui/material/Button";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [category, setCategory] = useState("todos");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (title && details && category) {
+      console.log(title, details, category);
+    }
+  };
+
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Notes
+      </Typography>
+
+      <form onSubmit={handleSubmit}>
+        <Box
+          sx={{ "& > :not(style)": { my: 3, width: "100%" } }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            onChange={(e) => setTitle(e.target.value)}
+            id="outlined-basic"
+            label="Note Title"
+            variant="outlined"
+            required
+          />
+
+          <TextField
+            onChange={(e) => setDetails(e.target.value)}
+            id="outlined-basic"
+            label="Details"
+            variant="outlined"
+            multiline
+            rows={4}
+            required
+          />
+        </Box>
+
+        <Box>
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">
+              Note Category
+            </FormLabel>
+            <RadioGroup
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel value="mony" control={<Radio />} label="Mony" />
+              <FormControlLabel
+                value="todos"
+                control={<Radio />}
+                label="Todos"
+              />
+              <FormControlLabel
+                value="reminder"
+                control={<Radio />}
+                label="Reminder"
+              />
+              <FormControlLabel value="word" control={<Radio />} label="Word" />
+            </RadioGroup>
+          </FormControl>
+        </Box>
+
+        <Box sx={{ "& button": { my: 2 } }}>
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            endIcon={<ArrowForwardIosIcon />}
+          >
+            Submit
+          </Button>
+        </Box>
+      </form>
+    </Container>
+  );
 }
